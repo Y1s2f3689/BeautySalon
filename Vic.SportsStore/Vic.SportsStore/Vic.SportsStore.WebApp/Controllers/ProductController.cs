ccd945981e9fae5ff02a870e49cd27f985309895
YSF <YSF>
2020-11-18 14:18:44 -0800

    1[CHG]add EF package

---
+++ Vic.SportsStore/Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vic.SportsStore.Domain.Abstract;
+using Vic.SportsStore.Domain.Concrete;
+
+namespace Vic.SportsStore.WebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IProductsRepository Productsrepository { get; set; }
+            = new InMemoryProductRepository();
+        public ViewResult List()
+        {
+            //M-V-C
+            //M -> Productsrepository.Products
+            //V -> View
+            //C -> ProductController
+
+            return View(Productsrepository.Products);
+        }
+       
+    }
+}
