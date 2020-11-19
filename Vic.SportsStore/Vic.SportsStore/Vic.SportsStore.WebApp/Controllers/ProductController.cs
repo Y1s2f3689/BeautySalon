d732a0fde8daec20cc03a048220165ec4f1efdc4
YSF <YSF>
2020-11-18 23:41:31 -0800

    1[NEW]add new files.

--- Vic.SportsStore/Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/ProductController.cs
+++ Vic.SportsStore/Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/ProductController.cs
@@ -5,12 +5,13 @@ using System.Web;
 using System.Web.Mvc;
 using Vic.SportsStore.Domain.Abstract;
 using Vic.SportsStore.Domain.Concrete;
+using Vic.SportsStore.WebApp.Models;
 
 namespace Vic.SportsStore.WebApp.Controllers
 {
     public class ProductController : Controller
     {
-        public IProductsRepository Productsrepository { get; set; }
+        public IProductsRepository ProductsRepository { get; set; }
             = new EFProductRepository();
 
         public int PageSize = 3;
@@ -18,23 +19,28 @@ namespace Vic.SportsStore.WebApp.Controllers
         {
             //1,2,3   4,5,6   7,8,9
             //M-V-C
-            //M -> Productsrepository.Products
+            //M -> ProductsRepository.Products
             //V -> View
             //C -> ProductController
 
-            var model = Productsrepository
-                .Products
-                .OrderBy(p => p.ProductId)
-                .Skip((page-1)*PageSize)
-                .Take(PageSize);
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = ProductsRepository
+                           .Products
+                           .OrderBy(p => p.ProductId)
+                           .Skip((page - 1) * PageSize)
+                           .Take(PageSize),
 
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = ProductsRepository.Products.Count()
+                }
+            };
             return View(model);
-               // repository
-               // .Products
-               // .OrderBy(p => p.ProductId)
-               // .Skip((page - 1) * PageSize)
-               // .Take(PageSize)); ;
         }
-       
+
     }
+
 }
