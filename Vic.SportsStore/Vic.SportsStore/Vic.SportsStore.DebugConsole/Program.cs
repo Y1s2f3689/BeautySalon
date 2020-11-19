dc71b68ae2e0d7af7d29a8b3448de7f9ac6735f9
YSF <YSF>
2020-11-18 18:33:01 -0800

    1[CHG] add EF EFDbContext.

--- Vic.SportsStore/Vic.SportsStore/Vic.SportsStore.DebugConsole/Program.cs
+++ Vic.SportsStore/Vic.SportsStore/Vic.SportsStore.DebugConsole/Program.cs
@@ -3,13 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vic.SportsStore.Domain.Concrete;
+using Vic.SportsStore.Domain.Entities;
 
 namespace Vic.SportsStore.DebugConsole
 {
     class Program
+
     {
         static void Main(string[] args)
         {
+            //List<Product> product = new List<Product>();
+
+            using (var ctx = new EFDbContext())
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    var product = new Product()
+                    {
+                        Name = $"football_{i}",
+                        Price = 12.34m,
+                        Description = "this is a football",
+                        Category = "ball"
+                    };
+
+                    ctx.Products.Add(product);
+
+                }
+
+                ctx.SaveChanges();
+            }
+
+            Console.WriteLine("done.");
+            Console.ReadLine();
         }
     }
 }
