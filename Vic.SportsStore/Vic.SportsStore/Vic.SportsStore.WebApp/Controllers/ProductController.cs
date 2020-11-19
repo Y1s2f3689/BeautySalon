c18f1b5a2b30225e12bdc5b26fbcac58ec5f9915
YSF <YSF>
2020-11-18 20:06:13 -0800

    1[CHG]add new files

--- Vic.SportsStore/Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/ProductController.cs
+++ Vic.SportsStore/Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/ProductController.cs
@@ -11,7 +11,7 @@ namespace Vic.SportsStore.WebApp.Controllers
     public class ProductController : Controller
     {
         public IProductsRepository Productsrepository { get; set; }
-            = new InMemoryProductRepository();
+            = new EFProductRepository();
         public ViewResult List()
         {
             //M-V-C
