dfca1ad4975f91db16ea97f46c6a31a70b9102bd
YSF <YSF>
2020-11-19 18:33:57 -0800

    1[NEW]add new files

---
+++ Jennifer.BeautySalon/Jennifer.BeautySalon.WebApp/Controllers/BeautyProductController.cs
@@ -0,0 +1,20 @@
+﻿using Jennifer.BeautySalon.Domain.Abstract;
+using Jennifer.BeautySalon.Domain.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Jennifer.BeautySalon.WebApp.Controllers
+{
+    public class BeautyProductController : Controller
+    {
+        public IBeautyProductsRepository BeautyProductsRepository { get; set; }
+            = new InMemoryBeautyProductRepository();
+        public ViewResult List()
+        {
+            return View(BeautyProductsRepository.BeautyProducts);
+        }
+    }
+}
