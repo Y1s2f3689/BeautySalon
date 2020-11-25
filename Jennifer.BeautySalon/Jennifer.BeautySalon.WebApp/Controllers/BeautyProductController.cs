b2590c595c6382f64be8ef56c6c0661991c89c06
YSF <YSF>
2020-11-25 12:07:08 -0800

    1[NEW]add new files.

--- Jennifer.BeautySalon/Jennifer.BeautySalon.WebApp/Controllers/BeautyProductController.cs
+++ Jennifer.BeautySalon/Jennifer.BeautySalon.WebApp/Controllers/BeautyProductController.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Jennifer.BeautySalon.Domain.Entities;
+using Jennifer.BeautySalon.WebApp.Models;
 
 namespace Jennifer.BeautySalon.WebApp.Controllers
 {
@@ -15,28 +17,29 @@ namespace Jennifer.BeautySalon.WebApp.Controllers
 
         public int PageSize = 3;
 
-        public ViewResult List(int page = 1);
-    }
-        BeautyProductsListViewModel model = new BeautyProductsListViewModel()
-
+        public ViewResult List(int page = 1)
         {
-            BeautyProducts = BeautyProductsRepository
-              .BeautyProducts
-              .OrderBy(p => p.BeautyProductId)
-              .Skip((page - 1) * PageSize)
-              .Take(PageSize),
 
-        PagingInfo = new PagingInfo
-           {
-              CurrentPage = page,
-              ItemsPerPage = PageSize,
-              TotalItems = BeautyProductsRepository.BeautyProducts.Count()
+            {
+                BeautyProductsListViewModel model = new BeautyProductsListViewModel
 
+                {
+                    BeautyProducts = BeautyProductsRepository
+                    .BeautyProducts
+                    .OrderBy(p => p.BeautyProductId)
+                    .Skip((page - 1) * PageSize)
+                    .Take(PageSize),
 
-           };
-            return View(model);
-         }
-        
-              
-    }
+                    PagingInfo = new PagingInfo
+                    {
+                        CurrentPage = page,
+                        ItemsPerPage = PageSize,
+                        TotalItems = BeautyProductsRepository.BeautyProducts.Count()
+                    };
+
+                return View(model);
+                }
+                
+            }
+        }
 }
---
+++ Jennifer.BeautySalon/Jennifer.BeautySalon.WebApp/Controllers/BeautyProductsListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Jennifer.BeautySalon.Domain.Entities;
+using System.Collections.Generic;
+
+namespace Jennifer.BeautySalon.WebApp.Controllers
+{
+    internal class BeautyProductsListViewModel
+    {
+        internal IEnumerable<BeautyProduct> BeautyProducts;
+
+        public PagingInfo PagingInfo { get; internal set; }
+    }
+}
